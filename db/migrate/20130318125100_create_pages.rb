e979878e8ac3c18a6fe1633cf1c8adfb7981215d
Josh Nussbaum <joshnuss@gmail.com>
2013-03-19 14:17:23 -0400

    adds create_pages migration

---
+++ db/migrate/20130318125100_create_pages.rb
@@ -0,0 +1,12 @@
+class CreatePages < ActiveRecord::Migration
+  def change
+    create_table :pages do |t|
+      t.string :title, :permalink
+      t.integer :parent_id
+      t.text :content
+      t.text :meta_description, :meta_keywords
+
+      t.timestamps
+    end
+  end
+end
