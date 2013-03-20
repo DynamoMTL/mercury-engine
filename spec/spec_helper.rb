4e32ad87c85ad20a184d72d1d16aef90fab22abd
Josh Nussbaum <joshnuss@gmail.com>
2013-03-20 10:22:19 -0400

    disables paper trail in test mode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,7 @@ require 'pry'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  PaperTrail.enabled = false
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
