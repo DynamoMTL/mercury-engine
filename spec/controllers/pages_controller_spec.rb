f076156bf8f9e2bc2e518ee57dc88b276d80c63d
Josh Nussbaum <joshnuss@gmail.com>
2013-03-28 14:38:34 -0400

    updates controller specs to use find_by_path without bang!

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,21 +8,22 @@ describe PagesController do
   end
 
   context "show" do
-    let(:page) { stub(:page, template_path: 'test') }
+    let(:page) { stub(:page) }
 
     context "without params" do
       before do
-        Page.should_receive(:find_by_path!).with(nil).and_return(page)
-        get :show
+        Page.should_receive(:find_by_path).with('').and_return(page)
+        get :show, path: ''
       end
 
-      specify { response.should be_success    }
+      specify { response.should be_success }
+      specify { response.should render_template("test") }
       specify { assigns(:page).should == page }
     end
 
     context "with params" do
       before do
-        Page.should_receive(:find_by_path!).with('about/company/services').and_return(page)
+        Page.should_receive(:find_by_path).with('about/company/services').and_return(page)
         get :show, path: 'about/company/services'
       end
 
