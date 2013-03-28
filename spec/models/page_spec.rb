ea53fc3505ee807df47bed6a3e937a5828b7cce1
Josh Nussbaum <joshnuss@gmail.com>
2013-03-28 14:56:52 -0400

    refactors page_spec

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -35,54 +35,58 @@ describe Page do
     context "root found" do
       let!(:root) { create_page(title: 'Home') }
 
-      specify('url empty')    { Page.find_or_create_by_path("").should == root }
-      specify('url is slash') { Page.find_or_create_by_path("/").should == root }
-      specify('url is nil')   { Page.find_or_create_by_path(nil).should == root }
+      specify('url empty')    { find_or_create("").should == root }
+      specify('url is slash') { find_or_create("/").should == root }
+      specify('url is nil')   { find_or_create(nil).should == root }
     end
 
     context "root no found" do
-      specify('url empty')    { Page.find_or_create_by_path("").path.should == "/" }
-      specify('url is slash') { Page.find_or_create_by_path("/").path.should == "/" }
-      specify('url is nil')   { Page.find_or_create_by_path(nil).path.should == "/" }
+      specify('url empty')    { find_or_create("").path.should == "/" }
+      specify('url is slash') { find_or_create("/").path.should == "/" }
+      specify('url is nil')   { find_or_create(nil).path.should == "/" }
     end
 
     context "child page found" do
       let!(:root)  { create_page(title: 'Home') }
       let!(:about) { create_page(title: 'About', parent: root) }
 
-      specify               { Page.find_or_create_by_path("about").should == about }
-      specify("with slash") { Page.find_or_create_by_path("/about").should == about }
+      specify               { find_or_create("about").should == about }
+      specify("with slash") { find_or_create("/about").should == about }
     end
 
     context "child page not found" do
       context "root found" do
         let!(:root) { create_page(title: 'Home') }
 
-        specify               { Page.find_or_create_by_path("about").parent.should == root }
-        specify               { Page.find_or_create_by_path("about").path.should == '/about' }
-        specify("with slash") { Page.find_or_create_by_path("/about").path.should == '/about' }
+        specify               { find_or_create("about").parent.should == root }
+        specify               { find_or_create("about").path.should   == '/about' }
+        specify("with slash") { find_or_create("/about").path.should  == '/about' }
       end
 
       context "root not found" do
-        specify               { Page.find_or_create_by_path("about").path.should == '/about' }
-        specify("with slash") { Page.find_or_create_by_path("/about").path.should == '/about' }
+        specify               { find_or_create("about").path.should  == '/about' }
+        specify("with slash") { find_or_create("/about").path.should == '/about' }
       end
     end
 
     context "grandchild page found" do
-      let!(:root)    { create_page(title: 'Home') }
-      let!(:about)   { create_page(title: 'About', parent: root) }
-      let!(:company) { create_page(title: 'Company', parent: about) }
+      let!(:root)           { create_page(title: 'Home') }
+      let!(:about)          { create_page(title: 'About', parent: root) }
+      let!(:company)        { create_page(title: 'Company', parent: about) }
 
-      specify               { Page.find_or_create_by_path("about/company").parent.should == about }
-      specify               { Page.find_or_create_by_path("about/company").should == company }
-      specify("with slash") { Page.find_or_create_by_path("/about/company").should == company }
+      specify               { find_or_create("about/company").parent.should == about }
+      specify               { find_or_create("about/company").should        == company }
+      specify("with slash") { find_or_create("/about/company").should       == company }
     end
 
     context "grandchild page not found" do
-      specify               { Page.find_or_create_by_path("about/company").path.should == '/about/company' }
-      specify("with slash") { Page.find_or_create_by_path("/about/company").path.should == '/about/company' }
+      specify               { find_or_create("about/company").path.should  == '/about/company' }
+      specify("with slash") { find_or_create("/about/company").path.should == '/about/company' }
     end
+  end
+
+  def find_or_create(path)
+    Page.find_or_create_by_path(path)
   end
 
   def create_page(attrs = {})
